ee553015b37024e9699f6fb9565810b007b63710
Muni <muneendra_yeddala@usjc.co.jp>
2025-11-28 22:00:21 +0900

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    // outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "docs/"),
     emptyOutDir: true,
   },
   // ,
