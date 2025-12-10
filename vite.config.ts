f7914ef3aa36fe535d96b4277e9dfe4462b52e6c
Muni <muneendra_yeddala@usjc.co.jp>
2025-12-10 22:25:58 +0900

    path issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const REPO_NAME = "/chris-route/";
-//const BASE_PATH = process.env.NODE_ENV === 'production' ? REPO_NAME : '/';
+const BASE_PATH = process.env.NODE_ENV === 'production' ? REPO_NAME : '/';
 // const BASE_PATH = REPO_NAME;
-const BASE_PATH = './';
+// const BASE_PATH = './';
 const PROJECT_ROOT = path.resolve(import.meta.dirname, "client");
 const OUTPUT_DIR = path.resolve(import.meta.dirname, "docs");
-
-//  const BASE_PATH = process.env.NODE_ENV === 'production' ? REPO_NAME : '/'; 
 
 
 export default defineConfig({
   base: BASE_PATH,
   plugins: [
     react(),
-    // runtimeErrorOverlay(),
-    // ...(process.env.NODE_ENV !== "production" &&
-    // process.env.REPL_ID !== undefined
-    //   ? [
-    //       await import("@replit/vite-plugin-cartographer").then((m) =>
-    //         m.cartographer(),
-    //       ),
-    //       await import("@replit/vite-plugin-dev-banner").then((m) =>
-    //         m.devBanner(),
-    //       ),
-    //     ]
-    //   : []),
   ],
   resolve: {
     alias: {
@@ -43,14 +28,4 @@ export default defineConfig({
     outDir: OUTPUT_DIR,
     emptyOutDir: true,
   },
-  // ,
-  //  preview: {
-  //   port: 5000, // <--- Add this to run 'npm run preview' on port 5000
-  // },
-  // server: {
-  //   fs: {
-  //     strict: true,
-  //     deny: ["**/.*"],
-  //   },
-  // },
 });
